48a043092a3b844a346ec8398040b5749f0ba78c
Iikka Hauhio <iikka.hauhio@helsinki.fi>
2020-01-30 18:04:33 +0200

    added option for lemmatising output

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,16 @@ use clap::{App, Arg};
 
 fn main() {
     let app = App::new("find-pairs")
-        .version("0.1.0")
+        .version("0.2.0")
         .author("Iikka Hauhio <iikka.hauhio@helsinki.fi>")
         .about("Find grammatical pairs in CONLL-U data")
         .arg(Arg::with_name("data").required(true))
-        .arg(Arg::with_name("rel").required(true));
+        .arg(Arg::with_name("rel").required(true))
+        .arg(Arg::with_name("lemmatise").short("l"));
     let matches = app.get_matches();
     let datafile = matches.value_of("data").unwrap();
     let rel = matches.value_of("rel").unwrap();
+    let lemmatise = matches.is_present("lemmatise");
 
     let reader = BufReader::new(File::open(datafile).expect("data file could not be opened"));
     let data = conllu_rs::parse_conllu(reader.lines().flatten());
@@ -19,7 +21,11 @@ fn main() {
     for sentence in data {
         for word in &sentence {
             if word.deprel.to_string() == rel {
-                println!("{} {}", word.form, sentence[word.head-1].form);
+                if lemmatise {
+                    println!("{} {}", word.lemma, sentence[word.head-1].lemma);
+                } else {
+                    println!("{} {}", word.form, sentence[word.head-1].form)
+                }
             }
         }
     }
